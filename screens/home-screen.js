01a3b9592fd0cc428b10596401a0b91b1ebdc8d9
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-09-23 18:05:03 -0400

    Update home-screen.js

--- screens/home-screen.js
+++ screens/home-screen.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import { Avatar } from 'react-native-elements';
 
 export default function HomeScreen() {
 
   const apiKey = '57df789ae4965f62a6a161f8b63d7642'
+
   const [myText, setMyText] = useState("My Original Text");
+  const [search, setSearch] = useState("");
 
   let getWeather = async (city) => {
     let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&units=imperial&appid=${apiKey}`)
@@ -20,10 +23,9 @@ export default function HomeScreen() {
     <View style={styles.container}>
          <SearchBar
         placeholder="Type Here..."
-        onChangeText=''
-        value=''
+        onChangeText={setSearch}
+        value={search}
         lightTheme={true}
-        style={styles.SearchBar}
       />
       <Text>{myText}</Text>
     </View>
@@ -34,11 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 
-  searchBar: {
-      width: '300px'
-  }
 });
